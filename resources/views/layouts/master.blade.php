ff0069cdcb2879580d9019dd2e9712383036cfa7
edoofra <edoardo.fra.ef@gmail.com>
2022-05-17 13:38:55 +0200

    create layout for top 10

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -30,18 +30,16 @@
         <li class="nav-item active">
           <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
+            Top 10
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Attaccanti</a>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
+            <a class="dropdown-item" href="#">Centrocampisti</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Difensori</a>
           </div>
         </li>
         <li class="nav-item">
