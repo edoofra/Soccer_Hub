33bfe0ba5c64f18e3f116403a815320421a5d646
edoofra <edoardo.fra.ef@gmail.com>
2022-05-22 10:14:25 +0200

    finish work on top10

--- resources/views/top10/Top_C.blade.php
+++ resources/views/top10/Top_C.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts/top10_layout')
 @section('titolo_top_10','CENTROCAMPISTI')
-@section('presentazione_sezione','This is the list of the 10 best strikers in the world. If you are looking for the star of your team you are in the right place!')
+@section('presentazione_sezione','This is the list of the 10 best midfielders in the world. If you are looking for the genius of your team you are in the right place!')
 @section('citazione',"The midfield is the hearthbeat of a football team, it's the centre of the action, so you can't just do one thing.")
 @section('citazione_autore','Casemiro')
 @section('caratteristica','Assist')
