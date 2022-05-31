378cac54b83c9fb7cbe494f1ff9be001b6cbf276
edoofra <edoardo.fra.ef@gmail.com>
2022-05-31 19:33:53 +0200

    small changes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,6 +15,14 @@
   <!-- jQuery e plugin JavaScript  -->
   <script src="http://code.jquery.com/jquery.js"></script>
   <script src="{{ url('/') }}/js/bootstrap.min.js"></script>
+  <!--
+  <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+  <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+  <script type="text/javascript" class="init">
+        $(document).ready(function () {
+        $('#example').DataTable();
+    });
+  </script> -->
 </head>
 
 <body>
