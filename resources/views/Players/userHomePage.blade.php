42d58543d5156a9d75bd3c0ac897ba762408d602
edoofra <edoardo.fra.ef@gmail.com>
2022-06-20 23:31:08 +0200

    added charts JS to dashboard

--- resources/views/Players/userHomePage.blade.php
+++ resources/views/Players/userHomePage.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.master')
 @section('script')
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.8.0/chart.min.js" integrity="sha512-sW/w8s4RWTdFFSduOTGtk4isV1+190E/GghVffMA9XczdJ2MDzSzLEubKAs5h0wzgSJOQTRYyaz73L3d6RtJSg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 @endsection
 @section('contenuto_pagina')
 
@@ -21,6 +22,14 @@
         <div class="col-md-12">
             <h1 class="h1" style="font-size:1.5em">ATTACCANTI</h1>
             <table class="table table-striped table-hover table-responsive" style="width:100%">
+                <col width='10%'>
+                <col width='10%'>
+                <col width='20%'>
+                <col width='10%'>
+                <col width='10%'>
+                <col width='10%'>
+                <col width='10%'>
+                <col width='10%'>
                 <thead>
                     <tr>
                         <th>Nome</th>
@@ -53,8 +62,56 @@
     </br>
     <div class="row">
         <div class="col-md-12">
+            <canvas id="myChart"></canvas>
+            <script>
+                const labels = [
+                    @foreach($listaAttaccanti as $attaccante)
+                    '{{ $attaccante->cognome }}',
+                    @endforeach
+                ];
+
+                gol = [
+                    @foreach($listaAttaccanti as $attaccante)
+                    {{ $attaccante->gol }},
+                    @endforeach
+                ];
+
+                const data = {
+                    labels: labels,
+                    datasets: [{
+                        label: 'GOALS',
+                        backgroundColor: 'rgb(255,127,80)',
+                        data: gol,
+                    }]
+                };
+
+                const config = {
+                    type: 'bar',
+                    data: data,
+                    options: {}
+                };
+            </script>
+            <script>
+                const myChart = new Chart(
+                document.getElementById('myChart'),
+                config
+             );
+            </script>
+        </div>
+    </div>
+    </br>
+    <div class="row">
+        <div class="col-md-12">
             <h1 class="h1" style="font-size:1.5em">CENTROCAMPISTI</h1>
             <table class="table table-striped table-hover table-responsive" style="width:100%">
+                <col width='10%'>
+                <col width='10%'>
+                <col width='20%'>
+                <col width='10%'>
+                <col width='10%'>
+                <col width='10%'>
+                <col width='10%'>
+                <col width='10%'>
                 <thead>
                     <tr>
                         <th>Nome</th>
@@ -87,8 +144,56 @@
     </br>
     <div class="row">
         <div class="col-md-12">
+            <canvas id="myChart1"></canvas>
+            <script>
+                const labels1 = [
+                    @foreach($listaCentrocampisti as $centrocampista)
+                    '{{ $centrocampista->cognome }}',
+                    @endforeach
+                ];
+
+                assist = [
+                    @foreach($listaCentrocampisti as $centrocampista)
+                    {{ $centrocampista->assist }},
+                    @endforeach
+                ];
+
+                const data1 = {
+                    labels: labels1,
+                    datasets: [{
+                        label: 'ASSIST',
+                        backgroundColor: 'rgb(255,127,80)',
+                        data: assist,
+                    }]
+                };
+
+                const config1 = {
+                    type: 'bar',
+                    data: data1,
+                    options: {}
+                };
+            </script>
+            <script>
+                const myChart1 = new Chart(
+                document.getElementById('myChart1'),
+                config1
+             );
+            </script>
+        </div>
+    </div>
+    </br>
+    <div class="row">
+        <div class="col-md-12">
             <h1 class="h1" style="font-size:1.5em">DIFENSORI</h1>
             <table class="table table-striped table-hover table-responsive" style="width:100%">
+                <col width='10%'>
+                <col width='10%'>
+                <col width='20%'>
+                <col width='10%'>
+                <col width='10%'>
+                <col width='10%'>
+                <col width='10%'>
+                <col width='10%'>
                 <thead>
                     <tr>
                         <th>Nome</th>
@@ -116,6 +221,46 @@
                     @endforeach
                 </tbody>
             </table>
+        </div>
+    </div>
+    </br>
+    <div class="row">
+        <div class="col-md-12">
+            <canvas id="myChart2"></canvas>
+            <script>
+                const labels2 = [
+                    @foreach($listaDifensori as $difensore)
+                    '{{ $difensore->cognome }}',
+                    @endforeach
+                ];
+
+                cl = [
+                    @foreach($listaDifensori as $difensore)
+                    {{ $difensore->clean_sheet }},
+                    @endforeach
+                ];
+
+                const data2 = {
+                    labels: labels2,
+                    datasets: [{
+                        label: 'CLEAN SHEETS',
+                        backgroundColor: 'rgb(255,127,80)',
+                        data: cl,
+                    }]
+                };
+
+                const config2 = {
+                    type: 'bar',
+                    data: data2,
+                    options: {}
+                };
+            </script>
+            <script>
+                const myChart2 = new Chart(
+                document.getElementById('myChart2'),
+                config2
+             );
+            </script>
         </div>
     </div>
     </br>
